02d9a5009c5eef222d643bf7c49fe589fac09854
yunjey <yunjey47@naver.com>
2016-09-24 00:33:29 +0900

    using pycocoevalcap

--- core/bleu.py
+++ core/bleu.py
@@ -1,64 +1,62 @@
-import nltk.translate.bleu_score as BLEU
-import numpy as np
 import cPickle as pickle
 import os
+import sys
+sys.path.append('../coco-caption')
+from pycocoevalcap.bleu.bleu import Bleu
+from pycocoevalcap.rouge.rouge import Rouge
+from pycocoevalcap.cider.cider import Cider
+from pycocoevalcap.meteor.meteor import Meteor
 
-def sentence_bleu(references, candidate, weights=[0.25, 0.25, 0.25, 0.25]):
-    # modified precision for n-gram
-    p_ns = [BLEU.modified_precision(references, candidate, i) for i, _ in enumerate(weights, start=1)]
-    
-    # all of n-gram modified precision should be positive otherwise return 0 (geometric average)
-    if all([x.numerator > 0 for x in p_ns]):
-        s = np.sum(w * np.log(float(p_n.numerator)/p_n.denominator) for w, p_n in zip(weights, p_ns))
-        
-        # brevity penalty 
-        c = len(candidate)
-        r = BLEU.closest_ref_length(references, c)
-        if c > r:
-            bp = 1
+def score(ref, hypo):
+    scorers = [
+        (Bleu(4),["Bleu_1","Bleu_2","Bleu_3","Bleu_4"]),
+        (Meteor(),"METEOR"),
+        (Rouge(),"ROUGE_L"),
+        (Cider(),"CIDEr")
+    ]
+    final_scores = {}
+    for scorer,method in scorers:
+        score,scores = scorer.compute_score(ref,hypo)
+        if type(score)==list:
+            for m,s in zip(method,score):
+                final_scores[m] = s
         else:
-            bp = np.exp(1. - float(r)/c)
-        return bp * np.exp(s)
-    else: 
-        return 0
-
-def bleu(targets, preds, n_gram):
-    '''
-    Inputs: 
-    - targets: reference sentences of shape (5000, 5)
-    - preds: candidate sentences of shape (5000, 1)
+            final_scores[method] = score
+    print 'Bleu_1:\t',final_scores['Bleu_1']  
+    print 'Bleu_2:\t',final_scores['Bleu_2']  
+    print 'Bleu_3:\t',final_scores['Bleu_3']  
+    print 'Bleu_4:\t',final_scores['Bleu_4']  
+    print 'METEOR:\t',final_scores['METEOR']  
+    print 'ROUGE_L:',final_scores['ROUGE_L']  
+    print 'CIDEr:\t',final_scores['CIDEr']  
     
-    Returns: 
-    - bleu score
-    '''
-    weights = []
-    w = 1. / n_gram
-    for _ in range(n_gram):
-        weights.append(w)
-    
-    bleu_score = 0
-    for i, pred in enumerate(preds):
-        pred = pred.split()
-        target = map(lambda x: x.split(), targets[i])
-        bleu_score += sentence_bleu(target, pred, weights)
-        
-    return bleu_score / len(preds)
-
-def compute_bleu_1to4(references, candidates):
-    # compute bleu1 ~ bleu4 scores
-    for i in range(4):
-        bleu_score = bleu(references, candidates, n_gram=i+1)
-        print "BLEU%d: %f" %(i+1, bleu_score)
 
 def evaluate(data_path='./data', split='val'):
-    # load candidates and references
-    data_path = os.path.join(data_path, split)
-    ref_cap_path = os.path.join(data_path, '%s.reference.captions.pkl' %split)
-    cand_cap_path = os.path.join(data_path, '%s.candidate.captions.pkl' %split)
+    reference_path = os.path.join(data_path, "%s/%s.reference.captions.pkl" %(split, split))
+    candidate_path = os.path.join(data_path, "%s/%s.candidate.captions.pkl" %(split, split))
+    
+    # load caption data
+    with open(reference_path, 'rb') as f:
+        ref = pickle.load(f)
+    with open(candidate_path, 'rb') as f:
+        cand = pickle.load(f)
+    
+    # make dictionary
+    hypo = {}
+    for i, caption in enumerate(cand):
+        hypo[i] = [caption]
+    
+    # compute bleu score
+    score(ref, hypo)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
-    with open(ref_cap_path, 'rb') as f:
-        references = pickle.load(f)
-    with open(cand_cap_path, 'rb') as f:
-        candidates = pickle.load(f)
 
-    compute_bleu_1to4(references, candidates)
