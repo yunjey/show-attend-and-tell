b041b90ef75c0c2b92624721e549b877c2d14bb0
yunjey <yunjey47@naver.com>
2016-10-13 14:24:16 +0900

    data generation file

--- data/data_generation.py
+++ data/data_generation.py
@@ -1,53 +1,29 @@
 import cPickle as pickle
-from preprocess import *
+from utils import *
 
 
-# load annotations
-with open('./train/train.annotations.pkl', 'rb') as f:
-    train_annotations = pickle.load(f)
-    
-    
-with open('./val/val.annotations.pkl', 'rb') as f:
-    val_annotations = pickle.load(f)
+def main():
+    splits = ['train', 'val', 'test']
+    for split in splits:
+        annotations = load_pickle('./%s/%s.annotations.pkl' %(split, split))
 
-    
-with open('./test/test.annotations.pkl', 'rb') as f:
-    test_annotations = pickle.load(f)
+        if split == 'train':
+            word_to_idx = load_pickle('./train/word_to_idx.pkl')
+            captions = build_caption_vectors(annotations, word_to_idx, 15)
+            save_pickle(captions, './train/train.captions.pkl')
 
-    
-with open('./train/word_to_idx.pkl', 'rb') as f:
-    word_to_idx = pickle.load(f)
-    
+        file_names, id_to_idx = get_file_names(annotations)
+        save_pickle(file_names, './%s/%s.file.names.pkl' %(split, split))
 
-train_captions = build_caption_vectors(train_annotations, word_to_idx, 15)
-with open('./train/train.captions.pkl', 'wb') as f:
-    pickle.dump(train_captions, f, pickle.HIGHEST_PROTOCOL)
-    print 'generated train.captions.pkl'
-    
+        if split == 'train':
+            image_idxs = get_image_idxs(annotations, id_to_idx)
+            save_pickle(image_idxs, './train/train.image.idxs.pkl')
 
-train_file_names, id_to_idx = get_file_names(train_annotations)
-with open('./train/train.file.names.pkl', 'wb') as f:
-    pickle.dump(train_file_names, f, pickle.HIGHEST_PROTOCOL)
-    print 'generated train.file.names.pkl'
-    
-    
-train_image_idxs = get_image_idxs(train_annotations, id_to_idx)
-with open('./train/train.captions.pkl', 'wb') as f:
-    pickle.dump(train_captions, f, pickle.HIGHEST_PROTOCOL)
-    print 'generated train.captions.pkl'
 
-    
-val_file_names, _ = get_file_names(val_annotations)
-with open('./val/val.file.names.pkl', 'wb') as f:
-    pickle.dump(val_file_names, f, pickle.HIGHEST_PROTOCOL)
-    print 'generated val.file.names.pkl'
+if __name__ == "__main__":
+   main()
 
-    
-test_file_names, _ = get_file_names(test_annotations)
-with open('./test/test.file.names.pkl', 'wb') as f:
-    pickle.dump(test_file_names, f, pickle.HIGHEST_PROTOCOL)
-    print 'generated test.file.names.pkl'
-    
+
     
     
     
