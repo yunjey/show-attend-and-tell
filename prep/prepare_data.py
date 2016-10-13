ebeaff02795a992a57001ff3261c0d52032a1a50
yunjey <yunjey47@naver.com>
2016-10-13 20:11:13 +0900

    prepare features and captions for training

--- prep/prepare_data.py
+++ prep/prepare_data.py
@@ -99,33 +99,20 @@ def get_image_idxs(annotations, id_to_idx):
 def main():
     splits = ['train', 'val', 'test']
     for split in splits:
-        annotations = load_pickle('../data/%s/%s.annotations.pkl' %(split, split))
+        annotations = load_pickle('./data/%s/%s.annotations.pkl' %(split, split))
 
         if split == 'train':
-            word_to_idx = load_pickle('../data/%s/word_to_idx.pkl' %split)
+            word_to_idx = load_pickle('./data/%s/word_to_idx.pkl' %split)
             captions = build_caption_vectors(annotations, word_to_idx, 15)
-            save_pickle(captions, '../data/%s/%s.captions.pkl' %(split, split))
+            save_pickle(captions, './data/%s/%s.captions.pkl' %(split, split))
 
         file_names, id_to_idx = get_file_names(annotations)
-        save_pickle(file_names, '../data/%s/%s.file.names.pkl' %(split, split))
+        save_pickle(file_names, './data/%s/%s.file.names.pkl' %(split, split))
 
         if split == 'train':
             image_idxs = get_image_idxs(annotations, id_to_idx)
-            save_pickle(image_idxs, '../data/%s/%s.image.idxs.pkl' %(split, split))
+            save_pickle(image_idxs, './data/%s/%s.image.idxs.pkl' %(split, split))
 
 
 if __name__ == "__main__":
-   main()
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    main()
