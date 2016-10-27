3a3f844cc0213a9bc42299a9ae5cb0196a34d71d
yunjey <yunjey47@naver.com>
2016-10-27 22:06:54 +0900

    it works

--- core/solver.py
+++ core/solver.py
@@ -6,7 +6,7 @@ import time
 import os 
 import cPickle as pickle
 from scipy import ndimage
-from utils import decode_captions, sample_coco_minibatch
+from utils import decode_captions, sample_coco_minibatch, write_bleu
 from bleu import evaluate
 
 
@@ -42,7 +42,7 @@ class CaptioningSolver(object):
             - word_to_idx: mapping dictionary from word to index 
         - val_data: validation data; for print out BLEU scores for each epoch.
         Optional Arguments:
-        - n_epochs: the number of epochs to run for during training.
+        - n_epochs: the number of epochs to run for training.
         - batch_size: mini batch size.
         - update_rule: a string giving the name of an update rule among the followings: 
             - 'sgd'
@@ -79,13 +79,7 @@ class CaptioningSolver(object):
         self.test_batch_size = kwargs.pop('test_batch_size', 100)
         self.candidate_caption_path = kwargs.pop('candidate_caption_path', './data/')
         self.print_bleu = kwargs.pop('print_bleu', False)
-
-        # Book-keeping variables 
-        self.best_model = None
-        self.loss_history = []
-        #self.best_val_acc = 0
-        #self.train_acc_history = []
-        #self.val_acc_history = []
+        self.log_path = kwargs.pop('log_path', './log/')
 
         # Set optimizer by update rule
         if self.update_rule == 'sgd':
@@ -99,33 +93,36 @@ class CaptioningSolver(object):
         elif self.update_rule == 'adadelta':
             self.optimizer = tf.train.AdadeltaOptimizer
         elif self.update_rule == 'rmsprop':
-            self.optimizer = tf.train.RMSPropOptimizer    # don't use this. it will cause "not implement error".
+            self.optimizer = tf.train.RMSPropOptimizer   
 
+
+        if not os.path.exists(self.model_path):
+        	os.makedirs(self.model_path)
+        if not os.path.exists(self.log_path):
+        	os.makedirs(self.log_path)
 
 
     def train(self):
         """
-        For given feature vectors and ground truth captions, solver trains model using gpu. 
-        solver also prints out some useful information such as loss and sampled caption.
-        For each epoch, model compares previous loss and current loss. 
-        If current loss is bigger than previous, then learning rate is decreased to half.
+        For given image features, solver trains model to generate captions similar to ground truths. 
+        Solver also prints out some useful information such as loss and generated captions.
         """
         n_examples = self.data['captions'].shape[0]
-        n_iters_per_epoch = int(np.ceil(float(n_examples) / self.batch_size))
+        n_iters_per_epoch = int(np.ceil(float(n_examples)/self.batch_size))
 
-        # training dataset
+        # Training dataset
         features = self.data['features']
         captions = self.data['captions']
         image_idxs = self.data['image_idxs']
         
-        # validation dataset
+        # Validation dataset
         val_features = self.val_data['features']
-        n_iters_val = int(np.ceil(float(val_features.shape[0]) / self.test_batch_size))
+        n_iters_val = int(np.ceil(float(val_features.shape[0])/self.test_batch_size))
 
-        # build graph for training
+        # Build graphs for training
         loss = self.model.build_model()
-        max_len = 20
-        _, generated_captions = self.model.build_sampler(max_len=max_len)
+        tf.get_variable_scope().reuse_variables()
+        _, generated_captions = self.model.build_sampler(max_len=20)
         optimizer = self.optimizer(self.learning_rate).minimize(loss)
 
         print "The number of epoch: %d" %self.n_epochs
@@ -143,11 +140,11 @@ class CaptioningSolver(object):
                 print "Start training with pretrained Model.."
                 saver.restore(sess, self.pretrained_model)
 
-            prev_loss = 1000000000
+            prev_loss = -1
             curr_loss = 0
             start_t = time.time()
             for e in range(self.n_epochs):
-                # random shuffle caption data
+                # Random shuffle caption data
                 rand_idxs = np.random.permutation(n_examples)
                 captions = captions[rand_idxs]
                 image_idxs = image_idxs[rand_idxs]
@@ -156,93 +153,53 @@ class CaptioningSolver(object):
                     image_idxs_batch = image_idxs[i*self.batch_size:(i+1)*self.batch_size]
                     features_batch = features[image_idxs_batch]
 
-                    # print initial loss
-                    if e == 0 and i == 0:
-                        feed_dict = { self.model.features: features_batch, self.model.captions: captions_batch }
-                        gen_caps, l = sess.run([generated_captions, loss], feed_dict)
-                        self.loss_history.append(l)
-                        print "\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
-                        print "Initial train loss: %.5f" %l
-                        decoded = decode_captions(gen_caps, self.model.idx_to_word)
-                        for j in range(1):
-                            print "Generated caption: %s" %decoded[j]
-                        print "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n"
-
-                    # optimize
+                    # Optimize
                     feed_dict = { self.model.features: features_batch, self.model.captions: captions_batch }
                     _, l = sess.run([optimizer, loss], feed_dict)
                     curr_loss += l
 
-                    # print info
                     if (i+1) % self.print_every == 0:
-                        # print out train loss 
-                        print "\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
-                        print "Train loss at epoch %d & iteration %d (mini-batch): %.5f" %(e+1, i+1, l)
-                        # print out ground truth
-                        
+                        # Print out mini-batch loss and ground truth captions
+                        print "\nTrain loss at epoch %d & iteration %d (mini-batch): %.5f" %(e+1, i+1, l)
                         ground_truths = captions[image_idxs == image_idxs_batch[0]]
                         decoded = decode_captions(ground_truths, self.model.idx_to_word)
                         for j, gt in enumerate(decoded):
-                            print "Ground truth %d: %s" %(j+1, gt)        
-                        # print out generated captions                       
+                            print "Ground truth %d: %s" %(j+1, gt)
+
+                        # Print out model-generated captions                       
                         gen_caps = sess.run(generated_captions, feed_dict)
                         decoded = decode_captions(gen_caps, self.model.idx_to_word)
-                        print "Generated caption: %s" %decoded[0]
-                        print "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n"
-                        # save loss history
-                        l = sess.run(loss, feed_dict)
-                        self.loss_history.append(l)
-    
+                        print "Generated caption: %s\n" %decoded[0]
+
                 print "Previous epoch loss: ", prev_loss
                 print "Current epoch loss: ", curr_loss
                 print "Elapsed time: ", time.time() - start_t
                 
-                # print out BLEU scores
+                # Print out BLEU scores for validation dataset
                 if self.print_bleu:
                     all_gen_cap = np.ndarray((val_features.shape[0], max_len))
                     for i in range(n_iters_val):
                         features_batch = val_features[i * self.test_batch_size:(i + 1) * self.test_batch_size]
                         feed_dict = {self.model.features: features_batch}
-                        gen_cap = sess.run(generated_captions, feed_dict=feed_dict)  # (N, max_len)
+                        gen_cap = sess.run(generated_captions, feed_dict=feed_dict)  
                         all_gen_cap[i * self.test_batch_size:(i + 1) * self.test_batch_size] = gen_cap
 
-                    # decode all sampled captions
+                    # Decode all sampled captions
                     all_decoded = decode_captions(all_gen_cap, self.model.idx_to_word)
                     with open(os.path.join(self.candidate_caption_path, "val/val.candidate.captions.pkl"), 'wb') as f:
                         pickle.dump(all_decoded, f, pickle.HIGHEST_PROTOCOL)
                         print "saved val.candidate.captions.pkl.."
                     
-                    # get bleu scores
-                    final_scores = evaluate(data_path='./data', split='val', get_scores=True)
-                    
-                    # file write
-                    if e == 0:
-                        file_mode = 'w'
-                    else:
-                        file_mode = 'a'
-                    with open(os.path.join(self.model_path, 'val.bleu.scores.txt'), file_mode) as f:
-                        f.write('Epoch %d\n' %(e+1))
-                        f.write('Bleu_1: %f\n' %final_scores['Bleu_1'])
-                        f.write('Bleu_2: %f\n' %final_scores['Bleu_2'])
-                        f.write('Bleu_3: %f\n' %final_scores['Bleu_3'])  
-                        f.write('Bleu_4: %f\n' %final_scores['Bleu_4']) 
-                        f.write('METEOR: %f\n' %final_scores['METEOR'])  
-                        f.write('ROUGE_L: %f\n' %final_scores['ROUGE_L'])  
-                        f.write('CIDEr: %f\n\n' %final_scores['CIDEr'])
+                    # Get BLEU scores and file write
+                    scores = evaluate(data_path='./data', split='val', get_scores=True)
+                    write_bleu(scores=scores, path=self.model_path, epoch=e)
+    
+                if (e+1) % self.save_every == 0:
+                    saver.save(sess, os.path.join(self.model_path, 'model'), global_step=e+1)
+                    print "model-%s saved." %(e+1)
                 
-                if prev_loss < curr_loss:
-                    #saver.restore(sess, os.path.join(self.model_path, self.best_model))
-                    self.learning_rate /= 2
-                    print "Reduce learning rate to %f..!" %self.learning_rate
-                else:
-                    prev_loss = curr_loss
-                    # save best model
-                    if (e+1) % self.save_every == 0:
-                        saver.save(sess, os.path.join(self.model_path, 'model'), global_step=e+1)
-                        print "model-%s saved." %(e+1)
-                        self.best_model = 'model-%s' %(e+1)
+                prev_loss = curr_loss
                 curr_loss = 0
-                print "Best model: %s" %self.best_model
         
                 
     def test(self, data, split='train', attention_visualization=True, save_sampled_captions=True):
@@ -256,16 +213,18 @@ class CaptioningSolver(object):
             - captions: captions of shape (24210, 17) 
             - image_idxs: indices for mapping caption to image of shape (24210, ) 
             - features_to_captions: mapping feature to captions (5000, 4~5)
+        - split: 'train', 'val' or 'test'
         '''
-        # get data
+
         features = data['features']
 
             
-        # build graph for sampling
+        # Build a graph for sampling captions
         max_len = 20
         alphas, sampled_captions = self.model.build_sampler(max_len)    # (N, max_len, L), (N, max_len)
         
         config = tf.ConfigProto(allow_soft_placement=True)
+        #config.gpu_options.per_process_gpu_memory_fraction=0.5
         config.gpu_options.allow_growth = True
         with tf.Session(config=config) as sess:
             # restore trained model
