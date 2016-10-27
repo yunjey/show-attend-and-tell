b64618a6d97346a8fefc1ab3b3c2bf88c7593feb
yunjey <yunjey47@naver.com>
2016-10-27 22:07:07 +0900

    it works

--- core/utils.py
+++ core/utils.py
@@ -94,3 +94,19 @@ def sample_coco_minibatch(data, batch_size):
     image_files = data['file_names'][image_idxs]
 
     return captions, features, image_files
+
+
+def write_bleu(scores, path, epoch):
+    if epoch == 0:
+        file_mode = 'w'
+    else:
+        file_mode = 'a'
+    with open(os.path.join(path, 'val.bleu.scores.txt'), file_mode) as f:
+        f.write('Epoch %d\n' %(epoch+1))
+        f.write('Bleu_1: %f\n' %scores['Bleu_1'])
+        f.write('Bleu_2: %f\n' %scores['Bleu_2'])
+        f.write('Bleu_3: %f\n' %scores['Bleu_3'])  
+        f.write('Bleu_4: %f\n' %scores['Bleu_4']) 
+        f.write('METEOR: %f\n' %scores['METEOR'])  
+        f.write('ROUGE_L: %f\n' %scores['ROUGE_L'])  
+        f.write('CIDEr: %f\n\n' %scores['CIDEr'])
