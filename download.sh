f10e761e1715be4d6c3ddef29b637b1e86450add
yunjey <yunjey47@naver.com>
2017-05-23 12:55:09 +0900

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@ wget http://www.vlfeat.org/matconvnet/models/imagenet-vgg-verydeep-19.mat -P dat
 wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip -P image/
 wget http://msvocds.blob.core.windows.net/coco2014/val2014.zip -P image/
 
-unzip captions_train-val2014.zip -d data/
+unzip data/captions_train-val2014.zip -d data/
 unzip image/train2014.zip -d image/
 rm image/train2014.zip 
 unzip image/val2014.zip -d image/ 
